1c7ca6774a649652ad8bbaaf6545602eee127055
Lane Seppala <lseppala@users.noreply.github.com>
2021-12-24 10:09:42 -0700

    Basic Maybe

--- maybe.go
+++ maybe.go
@@ -1,1 +1,19 @@
+package goehm
 
+type Maybe[T any] func(func(some T), func())
+
+func Some[T any](some T) (Maybe[T]) {
+	return Maybe[T](func(withSome func(some_ T), withNone func()){
+		withSome(some)
+	})
+}
+
+func None[T any]() (Maybe[T]) {
+	return Maybe[T](func(withSome func(some_ T), withNone func()){
+		withNone()
+	})
+}
+
+//func IsSome[T any](maybe Maybe[T]) bool {
+	//maybe(func(some T) { return true }, func() { return false} )
+//}
