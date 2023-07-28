feaac638e4b7a02655a8171e8e8775a06e8dbff9
Lane Seppala <lseppala@users.noreply.github.com>
2023-07-28 09:46:15 -0600

    add WIP for maybe

--- maybe.go
+++ maybe.go
@@ -1,19 +1,34 @@
 package goehm
 
-type Maybe[T any] func(func(some T), func())
+//type Maybe[T any] forall[T, any]
+//type forall[T any, R any] func(func(some T) R, func() R) R
 
-func Some[T any](some T) (Maybe[T]) {
-	return Maybe[T](func(withSome func(some_ T), withNone func()){
-		withSome(some)
-	})
-}
 
-func None[T any]() (Maybe[T]) {
-	return Maybe[T](func(withSome func(some_ T), withNone func()){
-		withNone()
-	})
-}
+//func Some[T any](some T) (Maybe[T]) {
+	//return Maybe[T](
+		//forall[T, R](
+			//func(withSome func(some_ T) R, withNone func()) R {
+				//return withSome(some)
+			//},
+		//),
+	//)
+//}
+
+//func None[T any]() (Maybe[T]) {
+	//return Maybe[T](func(withSome func(some_ T), withNone func()){
+		//withNone()
+	//})
+//}
 
 //func IsSome[T any](maybe Maybe[T]) bool {
-	//maybe(func(some T) { return true }, func() { return false} )
+	//return maybe(func(some T) { return true }, func() { return false} )
 //}
+
+
+type Maybe[T any] forall[T, any]
+type forall[T any, R any] struct{}
+
+
+func (forall forall[T, R]) Foo(withSome func(some T) R, none R) R {
+	return none
+}
